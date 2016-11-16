885be42100eeb4f260f31e2604fd5ed08dd3920e
Dave Kuntz <dkuntz811@gmail.com>
2016-11-15 18:14:43 -0600

    Hello React World works

---
+++ public/scripts/components.js
@@ -0,0 +1,11 @@
+"use strict";
+//
+// ReactDom.render(<div>Hello, React World</div>,
+// 	document.getElementById('helloworld')
+// );
+
+
+ReactDOM.render(
+  React.DOM.div(null, "Hello React World!"),
+  document.getElementById("HelloWorld")
+);
